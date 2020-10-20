659ce81df8d0f44b3e8c29dcb1c73692ed2ec5cd
Nate Sales <nate@natesales.net>
2020-10-20 16:28:15 -0700

    fix RTR server field

--- parser.go
+++ parser.go
@@ -312,7 +312,7 @@ func main() {
 	if config.RtrServer == "" {
 		config.RtrServer = "127.0.0.1"
 	}
-	log.Infof("Using RTR server %s", config.IrrDb)
+	log.Infof("Using RTR server %s", config.RtrServer)
 
 	// Validate Router ID in dotted quad format
 	if net.ParseIP(config.RouterId).To4() == nil {

4785ac8086349e5d4a2fea2be2d33d90888427b1
Nate Sales <nate@natesales.net>
2021-05-23 14:45:53 -0700

    add IRR query timeout

--- irr.go
+++ irr.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -19,7 +21,7 @@ func asSetToFilterName(asSet string, family uint8) string {
 }
 
 // Use bgpq4 to generate a prefix filter and return only the filter lines
-func getIRRPrefixSet(asSet string, family uint8, irrdb string) (string, error) {
+func getIRRPrefixSet(asSet string, family uint8, irrdb string, c *config) (string, error) {
 	if !(family == 4 || family == 6) {
 		log.Fatal("code error: getIRRPrefixSet family must be 4 or 6")
 	}
@@ -27,14 +29,12 @@ func getIRRPrefixSet(asSet string, family uint8, irrdb string) (string, error) {
 	// Run bgpq4 for BIRD format with aggregation enabled
 	cmdArgs := fmt.Sprintf("-h %s -Ab%d %s -l %s", irrdb, family, asSet, asSetToFilterName(asSet, family))
 	log.Infof("Running bgpq4 %s", cmdArgs)
-	cmd := exec.Command("bgpq4", strings.Split(cmdArgs, " ")...)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(c.IRRQueryTimeout))
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "bgpq4", strings.Split(cmdArgs, " ")...)
 	stdout, err := cmd.Output()
 	if err != nil {
 		return "", errors.New(fmt.Sprintf("bgpq4 error: %v, %s", err.Error(), stdout))
 	}
 	return "define " + string(stdout), nil // nil error
-}
-
-func main() {
-	fmt.Println(getIRRPrefixSet("AS34553:AS-ALL", 4, "rr.ntt.net"))
 }

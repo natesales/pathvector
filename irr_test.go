e02dd06f0e446a700ce68b76836ccd8f6d7d6c87
Nate Sales <nate@natesales.net>
2021-06-02 10:01:44 -0700

    add more IRR tests

--- irr_test.go
+++ irr_test.go
@@ -39,6 +39,8 @@ func TestGetIRRPrefixSet(t *testing.T) {
 		{"AS112", 4, "define PFXSET_AS112_IP4 = [192.31.196.0/24,192.175.48.0/24];", false},
 		{"AS112", 6, "define PFXSET_AS112_IP6 = [2001:4:112::/48,2620:4f:8000::/48];", false},
 		{"AS112", 9, "define PFXSET_AS112_IP6 = [2001:4:112::/48,2620:4f:8000::/48];", true}, // Invalid address family
+		{"AS-LROOT", 6, "define PFXSET_ASLROOT_IP6 = [2001:500:3::/48,2001:500:8c::/48,2001:500:9c::/47{47,48},2001:500:9e::/47,2001:500:9f::/48,2620:0:22b0::/48,2620:0:2ee0::/48];", false},
+		{"AS-FROOT", 4, "define PFXSET_ASFROOT_IP4 = [192.5.4.0/23{23,24},199.212.90.0/23,199.212.92.0/23,202.41.142.0/24];", false},
 	}
 	for _, tc := range testCases {
 		cliFlags.IRRQueryTimeout = 10

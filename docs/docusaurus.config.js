8ed4e442a34b3835d9fa1eec3256f3ece96d7f37
Nate Sales <nate@natesales.net>
2021-07-05 14:13:24 -0700

    docs: cleanup

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -61,13 +61,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/natesales/pathvector/edit/main/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/natesales/pathvector/edit/main/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

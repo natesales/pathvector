31c3794a6de07adc0e1aeec6f6e938ad035f93c8
Nate Sales <nate@natesales.net>
2021-07-05 17:54:47 -0700

    add TestOptimizerAcquisitionProgress

--- optimizer_test.go
+++ optimizer_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/go-ping/ping"
 	"testing"
 )
 
-func TestProbeSameAddressFamily(t *testing.T) {
+func TestOptimizerSameAddressFamily(t *testing.T) {
 	testCases := []struct {
 		a    string
 		b    string
@@ -19,6 +20,33 @@ func TestProbeSameAddressFamily(t *testing.T) {
 		out := sameAddressFamily(tc.a, tc.b)
 		if out != tc.same {
 			t.Errorf("a %s b %s expected same %v got %v", tc.a, tc.b, tc.same, out)
+		}
+	}
+}
+
+func TestOptimizerAcquisitionProgress(t *testing.T) {
+	testCases := []struct {
+		optimizer Optimizer
+		numPeers  int
+		expected  float64
+	}{
+		{Optimizer{CacheSize: 5}, 1, 0}, // Empty cache
+		{Optimizer{CacheSize: 5, Db: map[string][]probeResult{"Example": {
+			probeResult{Time: 0, Stats: ping.Statistics{}},
+			probeResult{Time: 1, Stats: ping.Statistics{}},
+			probeResult{Time: 2, Stats: ping.Statistics{}},
+			probeResult{Time: 3, Stats: ping.Statistics{}},
+			probeResult{Time: 4, Stats: ping.Statistics{}},
+		}}}, 1, 1}, // Full cache
+		{Optimizer{CacheSize: 5, Db: map[string][]probeResult{"Example": {
+			probeResult{Time: 0, Stats: ping.Statistics{}},
+			probeResult{Time: 1, Stats: ping.Statistics{}},
+		}}}, 1, 0.4}, // Partially full cache
+	}
+	for _, tc := range testCases {
+		out := acquisitionProgress(tc.optimizer, tc.numPeers)
+		if out != tc.expected {
+			t.Errorf("expected same %f got %f", tc.expected, out)
 		}
 	}
 }

9215f8690bd44c747090020920685e40a3cd9b9f
Nate Sales <nate@natesales.net>
2021-07-07 19:31:26 -0700

    test: move arista file

---
+++ vendorbuild/arista.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf dist/arista/
+mkdir dist/arista/
+echo "format: 1" > dist/arista/manifest.txt
+echo "primaryRpm: $$(ls dist/pathvector*linux-amd64.rpm)" >> dist/arista/manifest.txt
+for f in $$(ls dist/pathvector*linux-amd64.rpm); do echo "$$f-sha1: $$(sha1sum $$f | cut -d " " -f 1)"; done >> dist/arista/manifest.txt
+zip pathvector-$$(git describe --tags `git rev-list --tags --max-count=1` | cut -c2-)-arista-amd64.swix dist/arista/manifest.txt dist/pathvector*linux-amd64.rpm

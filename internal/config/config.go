0f085cd0233bbf3b3d2680cb33379452a758b9eb
Nate Sales <nate@natesales.net>
2021-08-21 21:43:52 -0700

    feat: add log file option

--- internal/config/config.go
+++ internal/config/config.go
@@ -187,6 +187,7 @@ type Config struct {
 	CacheDirectory        string `yaml:"cache-directory" description:"Directory to store runtime configuration cache" default:"/var/run/pathvector/cache/"`
 	KeepalivedConfig      string `yaml:"keepalived-config" description:"Configuration file for keepalived" default:"/etc/keepalived.conf"`
 	WebUIFile             string `yaml:"web-ui-file" description:"File to write web UI to (disabled if empty)" default:""`
+	LogFile               string `yaml:"log-file" description:"Log file location" default:"syslog"`
 
 	PortalHost string `yaml:"portal-host" description:"Peering portal host (disabled if empty)" default:""`
 	PortalKey  string `yaml:"portal-key" description:"Peering portal API key" default:""`

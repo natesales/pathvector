cca13a6e319aff199b233916cccd9491f8373a5b
Nate Sales <nate@natesales.net>
2021-10-24 16:41:24 -0700

    feat: add in-tree plugin documentation generator

--- docs/generate.sh
+++ docs/generate.sh
@@ -21,6 +21,16 @@ sidebar_position: 1
 ' >docs/docs/about.md
 cat README.md >>docs/docs/about.md
 rm /tmp/pathvector
+
+# Add peering portal readme page
 curl -s https://raw.githubusercontent.com/natesales/pathvector-portal/main/README.md >docs/docs/portal.md
 sed -i 's/# Pathvector Peering Portal/# Peering Portal/' docs/docs/portal.md
+
+# Add plugin readmes
+rm docs/docs/plugins/*.md
+for p in plugins/*/; do
+  plugin=$(echo "$p" | cut -d "/" -f 2)
+  cp "plugins/$plugin/README.md" "docs/docs/plugins/$plugin.md"
+done
+
 echo "done"

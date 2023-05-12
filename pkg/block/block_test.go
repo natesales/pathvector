718500121a6c1dfa0f79ac2d3a31a141ee56ec77
Nate Sales <nate@natesales.net>
2023-05-11 19:36:37 -0700

    fix: blocklist tests

--- pkg/block/block_test.go
+++ pkg/block/block_test.go
@@ -11,18 +11,17 @@ func TestBlockParse(t *testing.T) {
 		input       []string
 		asns        []uint32
 		prefixes    []string
-		ips         []string
 		shouldError bool
 	}{
-		{[]string{"AS65530", "AS65520", "192.0.2.0/24", "10.0.50.2"}, []uint32{65530, 65520}, []string{"192.0.2.0/24"}, []string{"10.0.50.2"}, false},
-		{[]string{"invalid", "AS65520", "192.0.2.0/24", "10.0.50.2"}, []uint32{}, []string{}, []string{}, true},
-		{[]string{"AS65530", "invalid", "192.0.2.0/24", "10.0.50.2"}, []uint32{}, []string{}, []string{}, true},
-		{[]string{"AS65530", "AS65520", "invalid", "10.0.50.2"}, []uint32{}, []string{}, []string{}, true},
-		{[]string{"AS65530", "AS65520", "192.0.2.0/24", "invalid"}, []uint32{}, []string{}, []string{}, true},
+		{[]string{"AS65530", "AS65520", "192.0.2.0/24", "10.0.50.2"}, []uint32{65530, 65520}, []string{"192.0.2.0/24", "10.0.50.2/32"}, false},
+		{[]string{"invalid", "AS65520", "192.0.2.0/24", "10.0.50.2"}, []uint32{}, []string{}, true},
+		{[]string{"AS65530", "invalid", "192.0.2.0/24", "10.0.50.2"}, []uint32{}, []string{}, true},
+		{[]string{"AS65530", "AS65520", "invalid", "10.0.50.2"}, []uint32{}, []string{}, true},
+		{[]string{"AS65530", "AS65520", "192.0.2.0/24", "invalid"}, []uint32{}, []string{}, true},
 	}
 
 	for _, tc := range tcs {
-		asns, prefixes, ips, err := Parse(tc.input)
+		asns, prefixes, err := Parse(tc.input)
 		if tc.shouldError {
 			assert.NotNil(t, err)
 			continue
@@ -31,11 +30,10 @@ func TestBlockParse(t *testing.T) {
 		}
 		assert.Equal(t, tc.asns, asns)
 		assert.Equal(t, tc.prefixes, prefixes)
-		assert.Equal(t, tc.ips, ips)
 	}
 }
 
 func TestBlockCombine(t *testing.T) {
 	combined := Combine([]string{"AS65530"}, []string{"https://raw.githubusercontent.com/natesales/pathvector/main/tests/blocklist.txt"}, []string{"../../tests/blocklist.txt"})
-	assert.Len(t, combined, 7)
+	assert.Len(t, combined, 19) // This only combines, doesn't sanitize so newlines and comments are included
 }

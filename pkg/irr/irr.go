b7de78d99169810305a92642b6dc6d9003ad24cb
Nate Sales <nate@natesales.net>
2023-07-28 23:39:48 -0700

    refactor: remove IRR source filter arg from bgpq4 (#192)

--- pkg/irr/irr.go
+++ pkg/irr/irr.go
@@ -26,22 +26,10 @@ func FirstASSet(asSet string) string {
 	return output
 }
 
-// withSourceFilter returns the AS set or AS set with the IRR source replaced with the -S SOURCE syntax
-// AS34553 -> AS34553
-// RIPE::AS34553 -> -S RIPE AS34553
-func withSourceFilter(asSet string) string {
-	if strings.Contains(asSet, "::") {
-		log.Debugf("Using IRRDB source from AS set %s", asSet)
-		tokens := strings.Split(asSet, "::")
-		return fmt.Sprintf("-S %s %s", tokens[0], tokens[1])
-	}
-	return asSet
-}
-
 // PrefixSet uses bgpq4 to generate a prefix filter and return only the filter lines
 func PrefixSet(asSet string, family uint8, irrServer string, queryTimeout uint, bgpqArgs string) ([]string, error) {
 	// Run bgpq4 for BIRD format with aggregation enabled
-	cmdArgs := fmt.Sprintf("-h %s -Ab%d %s", irrServer, family, withSourceFilter(asSet))
+	cmdArgs := fmt.Sprintf("-h %s -Ab%d %s", irrServer, family, asSet)
 	if bgpqArgs != "" {
 		cmdArgs = bgpqArgs + " " + cmdArgs
 	}
@@ -73,7 +61,7 @@ func PrefixSet(asSet string, family uint8, irrServer string, queryTimeout uint, 
 // ASMembers uses bgpq4 to generate an AS member set
 func ASMembers(asSet string, irrServer string, queryTimeout uint, bgpqArgs string) ([]uint32, error) {
 	// Run bgpq4 for BIRD format with aggregation enabled
-	cmdArgs := fmt.Sprintf("-h %s -tj %s", irrServer, withSourceFilter(asSet))
+	cmdArgs := fmt.Sprintf("-h %s -tj %s", irrServer, asSet)
 	if bgpqArgs != "" {
 		cmdArgs = bgpqArgs + " " + cmdArgs
 	}

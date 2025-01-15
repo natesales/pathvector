b4656925418f053eca67a21860ede94edf4af328
Nate Sales <nate@natesales.net>
2025-01-15 07:22:12 -0500

    chore: extract separate tests

--- cmd/optimizer_test.go
+++ cmd/optimizer_test.go
@@ -25,27 +25,29 @@ func TestOptimizer(t *testing.T) {
 	assert.GreaterOrEqual(t, 1, len(files))
 
 	for _, testFile := range files {
-		// Run pathvector to generate config first, so there is a config to modify
-		rootCmd.SetArgs(append(args, []string{
-			"generate",
-			"--config", testFile,
-		}...))
-		t.Logf("Running pre-optimizer generate: %v", args)
-		assert.Nil(t, rootCmd.Execute())
+		t.Run(testFile, func(t *testing.T) {
+			// Run pathvector to generate config first, so there is a config to modify
+			rootCmd.SetArgs(append(args, []string{
+				"generate",
+				"--config", testFile,
+			}...))
+			t.Logf("Running pre-optimizer generate: %v", args)
+			assert.Nil(t, rootCmd.Execute())
 
-		args = append(args, []string{
-			"optimizer",
-			"--config", testFile,
-		}...)
-		t.Logf("running probe integration with args %v", args)
-		rootCmd.SetArgs(args)
-		assert.Nil(t, rootCmd.Execute())
+			args = append(args, []string{
+				"optimizer",
+				"--config", testFile,
+			}...)
+			t.Logf("running probe integration with args %v", args)
+			rootCmd.SetArgs(args)
+			assert.Nil(t, rootCmd.Execute())
 
-		// Check if local pref is lowered
-		checkFile, err := os.ReadFile("/tmp/test-cache/AS65510_EXAMPLE.conf")
-		assert.Nil(t, err)
-		if !strings.Contains(string(checkFile), "bgp_local_pref = 80; # pathvector:localpref") {
-			t.Errorf("expected bgp_local_pref = 80 but not found in file")
-		}
+			// Check if local pref is lowered
+			checkFile, err := os.ReadFile("/tmp/test-cache/AS65510_EXAMPLE.conf")
+			assert.Nil(t, err)
+			if !strings.Contains(string(checkFile), "bgp_local_pref = 80; # pathvector:localpref") {
+				t.Errorf("expected bgp_local_pref = 80 but not found in file")
+			}
+		})
 	}
 }
